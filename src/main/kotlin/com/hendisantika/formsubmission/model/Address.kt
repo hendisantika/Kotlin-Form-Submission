e5b91b90fb373eb9dda86fcd0d61e980caaba8aa
Hendi Santika <hendisantika@gmail.com>
2017-12-19 07:41:13 +0700

    #3 - Add Address Entity

---
+++ src/main/kotlin/com/hendisantika/formsubmission/model/Address.kt
@@ -0,0 +1,17 @@
+package com.hendisantika.formsubmission.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-form-submission
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/12/17
+ * Time: 06.03
+ * To change this template use File | Settings | File Templates.
+ */
+
+data class Address(
+        var street: String? = null,
+        var postcode: String? = null
+)
