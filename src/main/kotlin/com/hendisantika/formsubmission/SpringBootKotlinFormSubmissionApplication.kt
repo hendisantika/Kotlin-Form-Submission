e490eb558162d9c650d0db6ba31a875ba4be1224
Hendi Santika <hendisantika@gmail.com>
2017-12-19 07:40:18 +0700

    #2 - Add Main App

---
+++ src/main/kotlin/com/hendisantika/formsubmission/SpringBootKotlinFormSubmissionApplication.kt
@@ -0,0 +1,22 @@
+package com.hendisantika.formsubmission
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-form-submission
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/12/17
+ * Time: 06.01
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+class SpringBootKotlinFormSubmissionApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringBootKotlinFormSubmissionApplication::class.java, *args)
+}
